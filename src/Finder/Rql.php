8360dd2f706f6fcf0d426da8a6d5ae5910a08f9b
Alex Lisenkov <alex@create.nl>
2019-10-18 12:42:14 +0200

    Upgrade dependencies

--- src/Finder/Rql.php
+++ src/Finder/Rql.php
@@ -50,6 +50,15 @@ class Rql implements FinderInterface
     public function filterQuery(): void {
         $rql = $this->request->get('rql');
 
-        ORMVisitorFactory::appendFiltersOnly($this->query, $rql);
+        if ($rql) {
+            ORMVisitorFactory::appendFiltersOnly($this->query, $rql);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function sortQuery(): void
+    {
     }
 }
